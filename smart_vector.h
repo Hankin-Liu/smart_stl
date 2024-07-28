302f54ba80e69e722c855d1e155c01e28f25a238
Hankin-Liu <wojiaoliuhuajun@126.com>
2024-07-28 21:40:19 +0800

    add smart_vector.h

---
+++ smart_vector.h
@@ -0,0 +1,72 @@
+/****************************************************************************************
+  > File Name: smart_vector.h
+  > Author: Liu Hua Jun
+  > Mail: wojiaoliuhuajun.com 
+  > Note: Use of this source code is governed by The GNU Affero General Public License
+          which can be found in the LICENSE file
+ ***************************************************************************************/
+#ifndef SMART_STL_VECTOR
+#define SMART_STL_VECTOR
+
+#include <vector>
+
+namespace smart_stl
+{
+
+template<typename T, typename Allocator = std::allocator<T>>
+class vector : public std::vector<T, Allocator>
+{
+public:
+    vector() {}
+    ~vector() {}
+public:
+    inline void push_back(const T& value) {
+        if (this->size() != this->capacity()) {
+            std::vector<T, Allocator>::push_back(value);
+            return;
+        }
+        realloc_insert(value);
+    }
+#if __cplusplus >= 201103L
+    inline void push_back(const T&& value) {
+        emplace_back(std::move(value));
+    }
+#endif
+
+#if __cplusplus >= 201103L
+    template<typename... _Args>
+    inline 
+#if __cplusplus > 201402L
+      typename vector<T, Allocator>::reference
+#else
+      void
+#endif
+      emplace_back(_Args&&... __args)
+      {
+        if (this->size() != this->capacity()) {
+            std::vector<T, Allocator>::emplace_back(__args...);
+        } else {
+            realloc_emplace(__args...);
+        }
+#if __cplusplus > 201402L
+        return back();
+#endif
+      }
+#endif
+
+private:
+    void __attribute__ ((noinline)) realloc_insert(const T& value) {
+        std::vector<T, Allocator>::push_back(value);
+    }
+#if __cplusplus >= 201103L
+    template<typename... _Args>
+    void __attribute__ ((noinline)) realloc_emplace(_Args&&... __args) {
+        std::vector<T, Allocator>::emplace_back(__args...);
+    }
+#endif
+
+private:
+};
+
+}
+#endif
